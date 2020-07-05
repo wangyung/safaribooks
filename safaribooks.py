f1d283a7ab149fe726433a59396dfb830ec5bfa5
Freddie Wang <wangyung@gmail.com>
2020-07-05 08:46:27 +0800

    Use book title for epub name.

--- safaribooks.py
+++ safaribooks.py
@@ -1058,7 +1058,7 @@ class SafariBooks:
             os.remove(zip_file + ".zip")
 
         shutil.make_archive(zip_file, 'zip', self.BOOK_PATH)
-        os.rename(zip_file + ".zip", os.path.join(self.BOOK_PATH, self.book_id) + ".epub")
+        os.rename(zip_file + ".zip", os.path.join(self.BOOK_PATH, self.book_title) + ".epub")
 
     def _start_coroutines(self, operation, *args):
         self.connection_futures.append(
